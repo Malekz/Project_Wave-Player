9fbfcdd9c7e2d9c6f78ee22731a07175fb12602f
Malek Zarzour <malek.zarzour@gmail.com>
2020-11-09 20:28:54 +0100

    initial commit

---
+++ src/components/player.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaPlay, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+
+      <div className="play-control">
+        <FaAngleLeft className="skip-back" />
+        <FaPlay className="play" />
+        <FaAngleRight className="skip-forward" />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
